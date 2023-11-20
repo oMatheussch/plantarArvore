0ea6ce9375710b1457721c95abf6545dab6a1d69
Matheus Schmidt <matisschmidt@gmail.com>
2023-11-20 20:08:32 -0300

    ESTILIZAÇÃO #01

--- carrosel.js
+++ carrosel.js
@@ -31,3 +31,41 @@ controls.forEach((control) => {
     items[currentItem].classList.add("current-item");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  let currentSlide = 0;
+  const slides = document.querySelectorAll("#cardsObjects");
+  const totalSlides = slides.length;
+
+  // Exibe o slide atual
+  function showSlide(index) {
+      slides.forEach((slide) => {
+          slide.style.display = "none";
+      });
+      slides[index].style.display = "block";
+  }
+
+  // Avança para o próximo slide
+  function nextSlide() {
+      currentSlide = (currentSlide + 1) % totalSlides;
+      showSlide(currentSlide);
+  }
+
+  // Retrocede para o slide anterior
+  function prevSlide() {
+      currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+      showSlide(currentSlide);
+  }
+
+  // Adiciona eventos aos botões de navegação
+  document.querySelector("#cards").addEventListener("click", function (event) {
+      if (event.target.classList.contains("prev-btn")) {
+          prevSlide();
+      } else if (event.target.classList.contains("next-btn")) {
+          nextSlide();
+      }
+  });
+
+  // Inicia o carrossel exibindo o primeiro slide
+  showSlide(currentSlide);
+});
